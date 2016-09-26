08e58e5ec9644e03a0764b095ee308d1b06c63e5
ahwong5 <ahwong5@gmail.com>
2016-09-26 23:34:38 +0800

    added enum role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:user, :restaurant_owner, :admin]
 end
