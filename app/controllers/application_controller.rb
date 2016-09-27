d4f9e7f8abff71ba1409d3ce3ff83fcd10310658
ahwong5 <ahwong5@gmail.com>
2016-09-27 10:23:37 +0800

    refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  # include Pundit
+  include Pundit
   protect_from_forgery with: :exception
 
-  # rescue_from Pundit::NotAuthorizedError do |exception|
-  #     flash[:danger] = "You're not authorized"
-  #     redirect_to request.referrer || root_path
-  #   end
+  rescue_from Pundit::NotAuthorizedError do |exception|
+      flash[:danger] = "You're not authorized"
+      redirect_to request.referrer || root_path
+    end
 end
