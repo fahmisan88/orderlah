cb568d360bc31a5a64522bbc1fa415f27d42a553
ahwong5 <ahwong5@gmail.com>
2016-09-27 12:38:08 +0800

    added Restaurant and meal Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+    resources :meals
+  end
+
+
 
   get :user_page, to: 'static_pages#user_page'
 end
