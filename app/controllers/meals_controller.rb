647ecb3b181821255753fdf5862a39fdce03c67d
fahmisan <ahmadfahmisan@gmail.com>
2016-09-27 11:04:29 +0800

    add meal controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -8,18 +8,40 @@ class MealsController < ApplicationController
   end
 
   def new
+    @meal = Meal.new
   end
 
   def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+
   end
 
   def edit
+    @meal = Meal.find(params[:id])
   end
 
   def update
+    @meal = Meal.find(params[:id])
+    if @meal.update(meal_params)
+      flash[:success]
+    else
+      flash[:danger]
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit( :name, :description, :price)
+
   end
 
 end
